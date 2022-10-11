228726911abcd6aa9f2790f282d29063f3a7de60
DiegoFusaro <diefusaro@gmail.com>
2022-10-11 12:54:27 -0300

    Final Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import './App.css'
 import Form from './components/Form'
 import UserCard from './components/UserCard'
+import { BsSun } from 'react-icons/bs'
+import { BsMoon } from 'react-icons/bs'
+
+export const ThemeContext = createContext(null);
 
 const baseURL = 'https://users-crud1.herokuapp.com'
 
 function App() {
 
-  const [formIsClose, setFormIsClose] = useState(false)
+  const [formIsClose, setFormIsClose] = useState(false);
   // Update the UserCard
-  const [updateInfo, setUpdateInfo] = useState()
-
-  const [users, setUsers] = useState()
+  const [updateInfo, setUpdateInfo] = useState();
+  // Obtiene al usuario
+  const [users, setUsers] = useState();
+  // Cambia el tema
+  const [theme, setTheme] = useState('light');
   // esta funcion nos realiza el GET de los users
   const getAllUsers = () => {
     const URL = `${baseURL}/users/`
     axios.get(URL)
       .then(res => setUsers(res.data))
       .catch(err => console.log(err))
-  }
+  };
 
   useEffect(() => {
     getAllUsers();
-  }, [])
+  }, []);
 
   // Crear usuario
-
   const createNewUser = data => {
     const URL = `${baseURL}/users/`
     axios.post(URL, data)
@@ -36,9 +41,7 @@ function App() {
         getAllUsers()
       })
       .catch(err => console.log(err))
-  }
-
-
+  };
 
   //Eliminar un usuario
   const deleteUserById = id => {
@@ -50,7 +53,7 @@ function App() {
         getAllUsers()
       })
       .catch(err => console.log(err))
-  }
+  };
 
   // Actualizar un usuario
   const updateUserById = (id, data) => {
@@ -61,41 +64,51 @@ function App() {
         getAllUsers()
       })
       .catch(err => console.log(err))
-  }
+  };
 
   const handleOpenForm = () => {
     setFormIsClose(true)
-  }
-
+  };
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+  };
   return (
-    <div className="App">
-      <div className="app__container--title">
-        <h1>Users Cards</h1>
-        <button onClick={handleOpenForm} className="modal__btn">Create Your Card</button>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className="App" id={theme}>
+        <div className="app__container--title">
+          <div className="light__mode">
+            <button onClick={toggleTheme} className='light__mode--icon'><BsSun /> || <BsMoon /></button>
+          </div>
+          <h1> User Cards Creator </h1>
+          <button onClick={handleOpenForm} className="modal__btn">Create Your Card</button>
+        </div>
+        <div className={`form__container ${formIsClose && 'form__disable'}`}>
+          <Form
+            createNewUser={createNewUser}
+            updateInfo={updateInfo}
+            updateUserById={updateUserById}
+            setUpdateInfo={setUpdateInfo}
+            setFormIsClose={setFormIsClose}
+          />
+        </div>
+        <div className='user__container'>
+          {
+            users?.map(user => (
+              <UserCard
+                key={user.id}
+                user={user}
+                deleteUserById={deleteUserById}
+                setUpdateInfo={setUpdateInfo}
+                setFormIsClose={setFormIsClose}
+              />
+            ))
+          }
+        </div>
+        <footer className='footer__container'>
+          <h3 className="footer__title">Copyright &#169; 2022 | All Rights Reserved</h3>
+        </footer>
       </div>
-      <div className={`form__container ${formIsClose && 'form__disable'}`}>
-        <Form
-          createNewUser={createNewUser}
-          updateInfo={updateInfo}
-          updateUserById={updateUserById}
-          setUpdateInfo={setUpdateInfo}
-          setFormIsClose={setFormIsClose}
-        />
-      </div>
-      <div className='user__container'>
-        {
-          users?.map(user => (
-            <UserCard
-              key={user.id}
-              user={user}
-              deleteUserById={deleteUserById}
-              setUpdateInfo={setUpdateInfo}
-              setFormIsClose={setFormIsClose}
-            />
-          ))
-        }
-      </div>
-    </div>
+    </ThemeContext.Provider>
   )
 }
 
